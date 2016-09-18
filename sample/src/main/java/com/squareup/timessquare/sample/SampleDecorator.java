08b3c81b99e9d31358f5f44082d654d9cbc80917
Quentin <vfishv@gmail.com>
2016-09-18 08:07:49 +0800

    m

--- sample/src/main/java/com/squareup/timessquare/sample/SampleDecorator.java
+++ sample/src/main/java/com/squareup/timessquare/sample/SampleDecorator.java
@@ -5,12 +5,16 @@ import android.text.Spanned;
 import android.text.style.RelativeSizeSpan;
 import com.squareup.timessquare.CalendarCellDecorator;
 import com.squareup.timessquare.CalendarCellView;
+
+import java.util.Calendar;
 import java.util.Date;
 
 public class SampleDecorator implements CalendarCellDecorator {
   @Override
   public void decorate(CalendarCellView cellView, Date date) {
-    String dateString = Integer.toString(date.getDate());
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
+    String dateString = Integer.toString(calendar.get(Calendar.DAY_OF_MONTH));
     SpannableString string = new SpannableString(dateString + "\ntitle");
     string.setSpan(new RelativeSizeSpan(0.5f), 0, dateString.length(),
         Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
